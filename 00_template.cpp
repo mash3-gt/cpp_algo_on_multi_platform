ae46d626e9117c2eb55d64697de51751213de41b
mashgt_dell_wsl <fg250j@gmail.com>
2024-11-06 23:58:58 +0900

    abc

--- 00_template.cpp
+++ 00_template.cpp
@@ -3,5 +3,22 @@ using namespace std;
 
 int main()
 {
+    // 入力
+    int a1, a2;
+    int b;
+    cin >> a1 >> a2;
+    cin >> b;
+
+    // for文
+
+    // if文
+
+    // while文
+
+    // set
+
+    // dict
+
+    // 出力
     cout << "hoge" << endl;
 }
