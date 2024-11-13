f3f8298b56595608f49d7cbd0cd44c2d9ed141b5
mash3gt <83237531+mash3-gt@users.noreply.github.com>
2024-11-13 17:50:06 +0000

    k

---
+++ apg4b/k.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+    //  graph of A
+    int i = 0;
+    cout << "A:";
+    while (i < A)
+    {
+        cout << "]";
+        i++;
+    }
+    cout << endl;
+    // graph of B
+    i = 0;
+    cout << "B:";
+    while (i < B)
+    {
+        cout << "]";
+        i++;
+    }
+    cout << endl;
+}
